3e344e2085f27068585d4dac5d9e298c51dc1563
Andrew42 <awightma@nd.edu>
2018-04-29 12:11:37 +0200

    Add MG26x patch to disable mass change check

--- mcgeneration/setup_production.sh
+++ mcgeneration/setup_production.sh
@@ -19,7 +19,7 @@ else
     git clone -b mg26x   https://github.com/cms-sw/genproductions.git genproductions 
     cd ${GENPRODPATH}
     # copy relevant code  
-    for FILE in addons runcmsgrid_LO.sh gridpack_generation.sh submit_madpack_ttbareft.sh ; do 
+    for FILE in addons patches runcmsgrid_LO.sh gridpack_generation.sh submit_madpack_ttbareft.sh ; do 
 	cp -r ${EFTMCPATH}/${FILE} ${GENPRODPATH}/.
     done
     cd ${GENPRODPATH}/.
